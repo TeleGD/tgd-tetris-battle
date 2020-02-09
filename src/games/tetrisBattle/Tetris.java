fb6c96f54f6e1fa98b2a6ce1395387f76dd867df
Paul Chipot <paul.chipot@telecomnancy.net>
2020-02-09 02:56:23 +0100

    Ajout d'un accesseur à Tetris.java

--- src/games/tetrisBattle/Tetris.java
+++ src/games/tetrisBattle/Tetris.java
@@ -5,20 +5,20 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Tetris {
-	
+
 	private List<Multimino> multiminos;
 	private List<List<Block>> blocks;
 	private List<Multimino> nextMultiminos;
-	
+
 	public Tetris() {
 		this.blocks = new LinkedList<List<Block>>();
-		
+
 		for (int k = 1; k < 21; k++) {
 			ArrayList<Block> line = new ArrayList<Block>(10);
 			blocks.add(line);
 		}
 	}
-	
+
 	private boolean completeLine(int indexLine) {
 		List<Block> line = blocks.get(indexLine);
 		for (int k = 0; k < 20; k++) {
@@ -28,13 +28,15 @@ public class Tetris {
 		}
 		return true;
 	}
-	
+
 	private void deleteLine(int indexLine) {
 		blocks.remove(indexLine);
 		ArrayList<Block> line = new ArrayList<Block>(10);
 		blocks.add(line);
 	}
-	
-	
+
+	public List<List<Block>> getBlocks() {
+		return this.blocks;
+	}
 
 }
