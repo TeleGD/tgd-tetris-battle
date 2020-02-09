a9512fefa498ce55c0fdad72ba16209d2f92391e
joannes-guichon <55346667+joannes-guichon@users.noreply.github.com>
2020-02-09 02:49:22 +0100

    Ajout du constructeur des différents multiminos

--- src/games/tetrisBattle/Multimino.java
+++ src/games/tetrisBattle/Multimino.java
@@ -1,12 +1,104 @@
 package games.tetrisBattle;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Multimino {
 	
-	private List<Block> blocks;
+	private List<List<Block>> blocks;
 	
-	public Multimino() {
+	public Multimino(String nameBlock) {
+		ArrayList<Block> l1 = new ArrayList<Block>();
+		ArrayList<Block> l2 = new ArrayList<Block>();
+		ArrayList<Block> l3 = new ArrayList<Block>();
+		ArrayList<Block> l4 = new ArrayList<Block>();
+		
+		switch(nameBlock) {
+			case "I":
+				this.blocks= new ArrayList<List<Block>>();
+				l1.add(null);l1.add(new Block());l1.add(null);l1.add(null);
+				l2.add(null);l2.add(new Block());l2.add(null);l2.add(null);
+				l3.add(null);l3.add(new Block());l3.add(null);l3.add(null);
+				l4.add(null);l4.add(new Block());l4.add(null);l4.add(null);
+				this.blocks.add(l1);
+				this.blocks.add(l2);
+				this.blocks.add(l3);
+				this.blocks.add(l4);
+				break;
+				
+			case "L":
+				this.blocks= new ArrayList<List<Block>>();
+				l1.add(null);l1.add(null);l1.add(null);l1.add(null);
+				l2.add(null);l2.add(new Block());l2.add(null);l2.add(null);
+				l3.add(null);l3.add(new Block());l3.add(null);l3.add(null);
+				l4.add(null);l4.add(new Block());l4.add(new Block());l4.add(null);
+				this.blocks.add(l1);
+				this.blocks.add(l2);
+				this.blocks.add(l3);
+				this.blocks.add(l4);
+				break;
+			
+			case "J":
+				this.blocks= new ArrayList<List<Block>>();
+				l1.add(null);l1.add(null);l1.add(null);l1.add(null);
+				l2.add(null);l2.add(null);l2.add(new Block());l2.add(null);
+				l3.add(null);l3.add(null);l3.add(new Block());l3.add(null);
+				l4.add(null);l4.add(new Block());l4.add(new Block());l4.add(null);
+				this.blocks.add(l1);
+				this.blocks.add(l2);
+				this.blocks.add(l3);
+				this.blocks.add(l4);
+				
+				break;
+			
+			case "O":
+				this.blocks= new ArrayList<List<Block>>();
+				l1.add(null);l1.add(null);l1.add(null);l1.add(null);
+				l2.add(null);l2.add(new Block());l2.add(new Block());l2.add(null);
+				l3.add(null);l3.add(new Block());l3.add(new Block());l3.add(null);
+				l4.add(null);l4.add(null);l4.add(null);l4.add(null);
+				this.blocks.add(l1);
+				this.blocks.add(l2);
+				this.blocks.add(l3);
+				this.blocks.add(l4);
+				break;
+			
+			case "Z":
+				this.blocks= new ArrayList<List<Block>>();
+				l1.add(null);l1.add(null);l1.add(null);l1.add(null);
+				l2.add(new Block());l2.add(new Block());l2.add(null);l2.add(null);
+				l3.add(null);l3.add(new Block());l3.add(new Block());l3.add(null);
+				l4.add(null);l4.add(null);l4.add(null);l4.add(null);
+				this.blocks.add(l1);
+				this.blocks.add(l2);
+				this.blocks.add(l3);
+				this.blocks.add(l4);
+				break;
+			
+			case "S":
+				this.blocks= new ArrayList<List<Block>>();
+				l1.add(null);l1.add(null);l1.add(null);l1.add(null);
+				l2.add(null);l2.add(null);l2.add(new Block());l2.add(new Block());
+				l3.add(null);l3.add(new Block());l3.add(new Block());l3.add(null);
+				l4.add(null);l4.add(new Block());l4.add(null);l4.add(null);
+				this.blocks.add(l1);
+				this.blocks.add(l2);
+				this.blocks.add(l3);
+				this.blocks.add(l4);
+				break;
+			
+			case "T":
+				this.blocks= new ArrayList<List<Block>>();
+				l1.add(null);l1.add(null);l1.add(null);l1.add(null);
+				l2.add(null);l2.add(new Block());l2.add(null);l2.add(null);
+				l3.add(null);l3.add(new Block());l3.add(new Block());l3.add(null);
+				l4.add(null);l4.add(new Block());l4.add(null);l4.add(null);
+				this.blocks.add(l1);
+				this.blocks.add(l2);
+				this.blocks.add(l3);
+				this.blocks.add(l4);
+				break;
+		}
 		
 	}
 
