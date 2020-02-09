fa0b8e135e7d492ca0a68ea82e79abb513300ffa
Evan <evan.weisse@telecomnancy.eu>
2020-02-09 02:15:21 +0100

    Constructeur et premières fonctions de la classe Tetris

--- src/games/tetrisBattle/Tetris.java
+++ src/games/tetrisBattle/Tetris.java
@@ -1,5 +1,7 @@
 package games.tetrisBattle;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Tetris {
@@ -9,7 +11,30 @@ public class Tetris {
 	private List<Block> nextBlocks;
 	
 	public Tetris() {
+		this.squares = new LinkedList<List<Square>>();
 		
+		for (int k = 1; k < 21; k++) {
+			ArrayList<Square> line = new ArrayList<Square>(10);
+			squares.add(line);
+		}
 	}
+	
+	private boolean completeLine(int indexLine) {
+		List<Square> line = squares.get(indexLine);
+		for (int k = 0; k < 20; k++) {
+			Square square = line.get(k);
+			if (square == null)
+				return false;
+		}
+		return true;
+	}
+	
+	private void deleteLine(int indexLine) {
+		squares.remove(indexLine);
+		ArrayList<Square> line = new ArrayList<Square>(10);
+		squares.add(line);
+	}
+	
+	
 
 }
